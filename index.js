54d6fb3257b55cb04cbb56e5701f0a57f135cc58
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2022-06-06 19:57:48 +0200

    with some trick and hack

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const options = {
 
 const observerContainers = new IntersectionObserver(function (entries, observerContainers) {
   entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    }
     const horLength = entry.target.children[0].scrollWidth
     const distFromTop = entry.target.parentNode.offsetTop;
     const scrollDistance = distFromTop + horLength - window.innerWidth;
+    console.log(horLength, distFromTop, scrollDistance);
+    if (!entry.isIntersecting) {
+      return;
+    }
     window.addEventListener('scroll', () => {
       let scrollTop = window.pageYOffset;
 
       if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
         entry.target.children[0].style.transform = "translateX(-" + (scrollTop - distFromTop) + "px)";
+
       }
     })
   });
